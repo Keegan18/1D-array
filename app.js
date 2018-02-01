1b21327a4dbebcee090203b278c543e78637eaf6
Keegan18 <30995446+Keegan18@users.noreply.github.com>
2018-01-31 14:40:02 -1000

    Add files via upload

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ function cars(){
 	var car3 = "Mazda";
 
 	//Create an array declaration named cars and set each car variable as the values.
-
+	var cars = [car1, car2, car3];
 	//Print the created array
-
+	console.log(cars);
 	//Print the array length
-
+	console.log(cars.length);
 	//Return the created array and the array length
-	return;
+	return (cars + "" + cars.length); 
 }
 
 console.log(cars());
@@ -28,13 +28,13 @@ console.log(cars());
 */
 function order(food, drink, dessert){
 	//Store the parameters in an array called "order"
-
+	var order = [food, drink, dessert];
 	//Print the array named "order"
-	
+	console.log(order);
 	//Print the length of the array named "order"
-
+	console.log(order.length);
 	//Return the order as a complete sentence represented as a string
-	return;
+	return ("I would like a " + order);
 }
 
 console.log(order("Burger", "root beer", "frosty"));
@@ -56,12 +56,15 @@ var currentClasses = [
 * Return 	classes (array, changed after iteration)
 */
 function loopToChange(classes){
+	var classes = 
 	//Print the parameter that is being passed through the function
-
+	console.log(classes);
 	//Use a For loop to iterate through each value in the passed array
-	
+	for (var i = currentClasses.length - 1; i >= 0; i--) {
+		currentClasses[i]
+	}
 	//Return classes
-	return;
+	return (classes);
 }
 
 console.log(loopToChange(currentClasses));
@@ -74,11 +77,11 @@ console.log(loopToChange(currentClasses));
 */
 function arrayMethodToLoop(numbers){
 	//Print the parameter that is being passed through the function
-
+	console.log(numbers);
 	//Use the forEach() method to add 5 to each value in the passed array
-	
+	numbers.forEach(arrayMethodToLoop())
 	//Return the changed array
-	return;
+	return (numbers);
 }
 
 console.log(arrayMethodToLoop([5, 7, 3, 4, 5]));
